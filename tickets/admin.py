1d6e39048c0f09dd165978e2814f3f52a15fd823
shishir-chowdhury <shiishir@gmail.com>
2025-08-18 16:13:03 +0600

    register model in admin

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Ticket, Comment, Attachment
 
-# Register your models here.
+class AttachmentInline(admin.TabularInline):
+    model = Attachment
+    extra = 0
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+    readonly_fields = ("created_at",)
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "status", "priority", "created_by", "assigned_to", "created_at")
+    list_filter = ("status", "priority", "assigned_to")
+    search_fields = ("title", "description")
+    inlines = [AttachmentInline, CommentInline]
+
+admin.site.register(Comment)
+admin.site.register(Attachment)
