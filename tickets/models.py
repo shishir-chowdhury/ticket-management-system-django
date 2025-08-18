3c2219573f3b3971244841f199b1c09f3533c1bd
shishir-chowdhury <shiishir@gmail.com>
2025-08-18 22:28:10 +0600

    add updated_at fields to the models

--- tickets/models.py
+++ tickets/models.py
@@ -23,6 +23,7 @@ class Ticket(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_tickets")
     due_date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
