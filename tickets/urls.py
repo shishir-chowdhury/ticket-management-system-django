db1b0d606a96f872189dc3351638d23e9d5e772b
shishir-chowdhury <shiishir@gmail.com>
2025-08-18 16:24:49 +0600

    add routes

---
+++ tickets/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import (
+    TicketListView, TicketCreateView, TicketDetailView, TicketUpdateView, AddCommentView
+)
+
+urlpatterns = [
+    path("", TicketListView.as_view(), name="ticket_list"),
+    path("tickets/new/", TicketCreateView.as_view(), name="ticket_create"),
+    path("tickets/<int:pk>/", TicketDetailView.as_view(), name="ticket_detail"),
+    path("tickets/<int:pk>/edit/", TicketUpdateView.as_view(), name="ticket_edit"),
+    path("tickets/<int:pk>/comment/", AddCommentView.as_view(), name="ticket_comment"),
+]
